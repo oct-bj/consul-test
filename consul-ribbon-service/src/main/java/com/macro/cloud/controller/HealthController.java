dfc9e077b8cbd52653ffefc58b9fb128f3d5a38b
litie <1794703060@qq.com>
2020-06-17 12:42:50 +0800

    增加health check endpoint

---
+++ consul-ribbon-service/src/main/java/com/macro/cloud/controller/HealthController.java
@@ -0,0 +1,23 @@
+package com.macro.cloud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.macro.cloud.domain.CommonResult;
+import com.macro.cloud.domain.User;
+
+@RestController
+@RequestMapping("/actuator")
+public class HealthController {
+	
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/health")
+    public CommonResult<User> getHealthState() {
+        LOGGER.info("收到Consul发过来的健康检查探针信息");
+        return new CommonResult<>();
+    }
+}
